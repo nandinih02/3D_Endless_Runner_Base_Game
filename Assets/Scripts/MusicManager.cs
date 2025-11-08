cfc7f823909fba78264ea74dd2d7f6ea385c8f41
nandinih02 <79862681+nandinih02@users.noreply.github.com>
2025-11-08 01:56:56 -0500

    Added Music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager instance = null;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
